aed50200f7ff50d4014eb52e3983c50d881ea143
richak <richakandlikar@gmail.com>
2013-10-02 22:48:23 -0400

    Next image display logic

--- src/com/example/hw4/PhotoActivity.java
+++ src/com/example/hw4/PhotoActivity.java
@@ -1,8 +1,6 @@
 package com.example.hw4;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ExecutorService;
@@ -29,40 +27,43 @@ public class PhotoActivity extends Activity {
 	URL url;
 	String s[];
 	Bitmap bm;
-	int mode=0;
+	int mode = 0;
 	int cur_image, size;
-	float nextXClick,prevXClick,maxX,minX,maxY,minY;
+	float nextXClick, prevXClick, maxX, minX, maxY, minY;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_photo);
-		
-		s= getResources().getStringArray(R.array.photo_urls);
-		
+
+		s = getResources().getStringArray(R.array.photo_urls);
+
 		ivMain = (ImageView) findViewById(R.id.ivMain);
 		ivMain.setScaleType(ScaleType.FIT_CENTER);
-		
+
 		if (getIntent().getExtras() != null) {
 			mode = getIntent().getExtras().getInt("MODE");
 			cur_image = 0;
 			size = s.length;
-			new DoWork().execute();
 		}
 
-		if(mode==1){
+		if (mode == 1) {
+			loadImage();
 			ivMain.setOnTouchListener(new OnTouchListener() {
-	
+
 				public boolean onTouch(View v, MotionEvent event) {
-					Log.d("DEBUG","Touched");
-					//TODO: view.setImageBitmap(imageloader.next());
+					Log.d("DEBUG", "Touched");
+					// TODO: view.setImageBitmap(imageloader.next());
+					nextImage();
 					return false;
 				}
 			});
-		}
-		else{
-			//TODO: init a slideshow with 2 second interval.
-			// on2secondInterval -> view.setImageBitmap(imageloader.next());
+		} else {
+			loadImage();
+			while(true){
+			//	wait(2);
+				nextImage();
+			}
 		}
 	}
 
@@ -73,13 +74,27 @@ public class PhotoActivity extends Activity {
 		return true;
 	}
 
-	class DoWork extends AsyncTask<Void, Integer, Void> {
+	public void loadImage() {
+		new ImageLoader().execute();	
+	}
+
+	public void nextImage() {
+		cur_image = (cur_image + 1) % size;
+		loadImage();
+	}
+
+	public void prevImage() {
+		cur_image = (cur_image - 1) % size;
+		loadImage();
+	}
+
+	class ImageLoader extends AsyncTask<Void, Integer, Void> {
+
 
 		@Override
 		protected Void doInBackground(Void... params) {
-
 			try {
-				InputStream in = new java.net.URL(s[13]).openStream();
+				InputStream in = new java.net.URL(s[cur_image]).openStream();
 				bm = BitmapFactory.decodeStream(in);
 			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
@@ -88,7 +103,6 @@ public class PhotoActivity extends Activity {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
 			return null;
 		}
 
